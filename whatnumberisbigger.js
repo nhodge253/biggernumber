8bbc75e460fcc0ab3f862043867b21d61650c710
Nate Hodge <nhodge253@gmail.com>
2019-09-05 18:07:01 -0700

    first commit

---
+++ whatnumberisbigger.js
@@ -0,0 +1,12 @@
+var first = Number(prompt("Please enter a number."));
+var second = Number(prompt("Please enter another number."));
+
+    if (first > second) {
+        alert(first + " is the bigger number.");
+    }
+    else if (second > first){
+        alert(second + " is the bigger number");
+    }
+    else {alert ("These numbers are equal.");
+}
+
